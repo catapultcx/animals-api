e5ee38ddcfa99a5b822745e116022ca7c7ea09bd
gary <info@gddevltd.co.uk>
2021-04-29 12:51:53 +0100

    Adding Crustacean domain class

---
+++ src/main/java/cx/catapult/animals/domain/Crustacean.java
@@ -0,0 +1,11 @@
+package cx.catapult.animals.domain;
+
+public class Crustacean extends BaseAnimal {
+  public Crustacean() {
+    this("", "");
+  }
+
+  public Crustacean(String name, String description) {
+    super(name, description, Group.INVERTEBRATE);
+  }
+}
