b235de68c62b34f0d8f17027eb6ee4551ba5274f
llmikeyj <mike.jackson@benezet.co.uk>
2022-12-18 17:39:59 +0000

    1: Store Animal Information: Add controller

---
+++ src/main/java/cx/catapult/animals/web/AnimalsController.java
@@ -0,0 +1,44 @@
+package cx.catapult.animals.web;
+
+import cx.catapult.animals.domain.BaseAnimal;
+import cx.catapult.animals.service.AnimalsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping(path = "/api/1/animals", produces = MediaType.APPLICATION_JSON_VALUE)
+public class AnimalsController {
+
+    private final AnimalsService animalsService;
+
+    @Autowired
+    public AnimalsController(AnimalsService animalsService) {
+        this.animalsService = animalsService;
+    }
+
+    @GetMapping(value = "")
+    public @ResponseBody
+    Collection<BaseAnimal> all() {
+        return animalsService.all();
+    }
+
+    @GetMapping(value = "/{id}")
+    public @ResponseBody
+    BaseAnimal get(@PathVariable String id) {
+        return animalsService.get(id);
+    }
+
+    @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public @ResponseBody
+    BaseAnimal create(@RequestBody BaseAnimal animal) {
+        return animalsService.create(animal);
+    }
+
+
+
+}
