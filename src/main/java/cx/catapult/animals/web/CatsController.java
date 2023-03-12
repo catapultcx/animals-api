14cf15ee8b3e53804b6d90bdbd88e6203ff583f2
kunteper <kunteper@kt.com>
2023-03-12 23:01:20 +0000

    Endpoints added

--- src/main/java/cx/catapult/animals/web/CatsController.java
+++ src/main/java/cx/catapult/animals/web/CatsController.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Collection;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api/1/cats", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -18,7 +20,12 @@ public class CatsController {
 
     @GetMapping(value = "", produces = "application/json")
     public @ResponseBody
-    Collection<Cat> all() {
+    Collection<Cat> all(@RequestParam Optional<String> name, @RequestParam Optional<String> description) {
+        if(name.isPresent() || description.isPresent())
+            return service.getByFilter(
+                name.isPresent() ? name.get() : "",
+                description.isPresent() ? description.get() : ""
+            );
         return service.all();
     }
 
@@ -32,7 +39,20 @@ public class CatsController {
     @ResponseStatus(HttpStatus.CREATED)
     public @ResponseBody
     Cat
-    create(@RequestBody Cat cat) {
+    create(@Valid @RequestBody Cat cat) {
         return service.create(cat);
     }
+
+    @PutMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody Cat update(@Valid @RequestBody Cat cat) {
+        if(cat.getId()==null || cat.getId().isEmpty())
+            return cat;
+        return service.update(cat);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public boolean delete(@PathVariable String id) {
+        return service.delete(id);
+    }
+
 }
