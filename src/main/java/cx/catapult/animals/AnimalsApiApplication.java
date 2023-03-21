a315f360b0b53150a25212841e5006fc482c0054
Anandarao Posimsetti <Anandarao.Posimsetti@external.t-systems.com>
2023-03-21 12:32:37 +0000

    UserStory-SSR removing CORS filter as SSR is implemented

--- src/main/java/cx/catapult/animals/AnimalsApiApplication.java
+++ src/main/java/cx/catapult/animals/AnimalsApiApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 
@@ -27,16 +25,6 @@ public class AnimalsApiApplication {
 //			for (String beanName : beanNames) {
 //				System.out.println(beanName);
 //			}
-		};
-	}
-
-	@Bean
-	public WebMvcConfigurer webMvcConfigurer(){
-		return  new WebMvcConfigurer(){
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET","POST","PUT","DELETE");
-			}
 		};
 	}
 
