cd94facc50037acc2858e63a3c97dbaa561c26b0
wisniewskib <bartoszwisniewski185@gmail.com>
2023-03-23 15:57:02 +0000

    test: Add test to check for exceptions in service layer

--- src/test/java/cx/catapult/animals/service/CatsServiceTest.java
+++ src/test/java/cx/catapult/animals/service/CatsServiceTest.java
@@ -1,50 +1,75 @@
 package cx.catapult.animals.service;
 
-import cx.catapult.animals.domain.Cat;
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.web.server.ResponseStatusException;
+
+import cx.catapult.animals.domain.Cat;
+import cx.catapult.animals.exception.AnimalNotFoundException;
 
 public class CatsServiceTest {
 
-    CatsService service = new CatsService();
-    Cat cat = new Cat("Tom", "Bob cat");
+	CatsService service = new CatsService();
+	Cat cat = new Cat("Tom", "Bob cat");
 
-    @Test
-    public void createShouldWork() throws Exception {
-        Cat thisCat = new Cat();
-        thisCat.setName("Jerry");
-        thisCat.setDescription("Mouse Cat");
-        Cat actual = service.create(thisCat);
-        assertThat(actual).isEqualTo(thisCat);
-        assertThat(actual.getName()).isEqualTo(thisCat.getName());
-        assertThat(actual.getDescription()).isEqualTo(thisCat.getDescription());
-        assertThat(actual.getGroup()).isEqualTo(thisCat.getGroup());
-    }
+	@Test
+	public void createShouldWork() throws Exception {
+		Cat thisCat = new Cat();
+		thisCat.setName("Jerry");
+		thisCat.setDescription("Mouse Cat");
+		Cat actual = service.create(thisCat);
+		assertThat(actual).isEqualTo(thisCat);
+		assertThat(actual.getName()).isEqualTo(thisCat.getName());
+		assertThat(actual.getDescription()).isEqualTo(thisCat.getDescription());
+		assertThat(actual.getGroup()).isEqualTo(thisCat.getGroup());
+	}
 
-    @Test
-    public void allShouldWork() throws Exception {
-        service.create(cat);
-        assertThat(service.all().size()).isEqualTo(1);
-    }
+	@Test
+	public void createShouldFailIfNameOrDescriptionIsMissing() throws Exception {
+		Cat thisCat = new Cat();
+		assertThatThrownBy(() -> service.create(thisCat)).isInstanceOf(IllegalArgumentException.class)
+				.hasMessageContaining("Name and description are required fields");
+	}
 
-    @Test
-    public void getShouldWork() throws Exception {
-        service.create(cat);
-        Cat actual = service.get(cat.getId());
-        assertThat(actual).isEqualTo(cat);
-        assertThat(actual.getName()).isEqualTo(cat.getName());
-        assertThat(actual.getDescription()).isEqualTo(cat.getDescription());
-        assertThat(actual.getGroup()).isEqualTo(cat.getGroup());
-    }
-    
-    @Test
-    public void deleteShouldWork() throws Exception {
-        service.create(cat);
+	@Test
+	public void allShouldWork() throws Exception {
+		service.create(cat);
+		assertThat(service.all().size()).isEqualTo(1);
+	}
 
-        service.delete(cat.getId());
+	@Test
+	public void getShouldWork() throws Exception {
+		service.create(cat);
+		Cat actual = service.get(cat.getId());
+		assertThat(actual).isEqualTo(cat);
+		assertThat(actual.getName()).isEqualTo(cat.getName());
+		assertThat(actual.getDescription()).isEqualTo(cat.getDescription());
+		assertThat(actual.getGroup()).isEqualTo(cat.getGroup());
+	}
 
-        assertThat(service.get(cat.getId())).isNull();
-        assertThat(service.all().size()).isEqualTo(0);
-    }
+	@Test
+	public void getShouldFailIfIdIsInvalid() {
+		String id = "id";
+		assertThatThrownBy(() -> service.get(id)).isInstanceOf(AnimalNotFoundException.class)
+				.hasMessageContaining(String.format("Animal not found with id: %s", id));
+	}
+
+	@Test
+	public void deleteShouldWork() throws Exception {
+		service.create(cat);
+
+		service.delete(cat.getId());
+
+		assertThat(service.get(cat.getId())).isNull();
+		assertThat(service.all().size()).isEqualTo(0);
+	}
+
+	@Test
+	public void deleteShouldFail() throws Exception {
+		String id = "id";
+		assertThatThrownBy(() -> service.delete(id)).isInstanceOf(AnimalNotFoundException.class)
+				.hasMessageContaining(String.format("Animal not found with id:  %s", id));
+	}
 }
