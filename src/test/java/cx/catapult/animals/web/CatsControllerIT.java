b1349d96cabeea27909a0ae96d6d99e486e9af55
wisniewskib <bartoszwisniewski185@gmail.com>
2023-03-23 18:47:38 +0000

    test: Add Integration tests and controller tets

--- src/test/java/cx/catapult/animals/web/CatsControllerIT.java
+++ src/test/java/cx/catapult/animals/web/CatsControllerIT.java
@@ -1,67 +1,129 @@
 package cx.catapult.animals.web;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
-import cx.catapult.animals.domain.Cat;
+import java.net.URL;
+import java.util.Collection;
+
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
-import java.net.URL;
-import java.util.Collection;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
+import cx.catapult.animals.domain.Cat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 
 public class CatsControllerIT {
-    @LocalServerPort
-    private int port;
+	@LocalServerPort
+	private int port;
 
-    private URL base;
+	private URL base;
 
-    private Cat cat = new Cat("Tom", "Bob cat");
+	private Cat cat = new Cat("Tom", "Bob cat");
 
-    @Autowired
-    private TestRestTemplate template;
+	@Autowired
+	private TestRestTemplate template;
 
-    @BeforeEach
-    public void setUp() throws Exception {
-        this.base = new URL("http://localhost:" + port + "/api/1/cats");
-    }
+	@BeforeEach
+	public void setUp() throws Exception {
+		this.base = new URL("http://localhost:" + port + "/api/1/cats");
+	}
 
-    @Test
-    public void createShouldWork() throws Exception {
-        ResponseEntity<Cat> response = template.postForEntity(base.toString(), cat, Cat.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-        assertThat(response.getBody().getId()).isNotEmpty();
-        assertThat(response.getBody().getName()).isEqualTo(cat.getName());
-        assertThat(response.getBody().getDescription()).isEqualTo(cat.getDescription());
-        assertThat(response.getBody().getGroup()).isEqualTo(cat.getGroup());
-    }
+	@Test
+	public void createShouldWork() throws Exception {
+		ResponseEntity<Cat> response = template.postForEntity(base.toString(), cat, Cat.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+		assertThat(response.getBody().getId()).isNotEmpty();
+		assertThat(response.getBody().getName()).isEqualTo(cat.getName());
+		assertThat(response.getBody().getDescription()).isEqualTo(cat.getDescription());
+		assertThat(response.getBody().getGroup()).isEqualTo(cat.getGroup());
+	}
 
-    @Test
-    public void allShouldWork() throws Exception {
-        Collection items = template.getForObject(base.toString(), Collection.class);
-        assertThat(items.size()).isGreaterThanOrEqualTo(7);
-    }
+	@Test
+	public void createShouldFail() throws Exception {
+	    // Create a JSON object with a missing "name" field
+	    JSONObject json = new JSONObject();
+	    json.put("description", "Cat without name");
+	    
+	    HttpHeaders headers = new HttpHeaders();
+	    headers.setContentType(MediaType.APPLICATION_JSON);
+	    HttpEntity<String> request = new HttpEntity<>(json.toString(), headers);
 
-    @Test
-    public void getShouldWork() throws Exception {
-        Cat created = create("Test 1");
-        ResponseEntity<String> response = template.getForEntity(base.toString() + "/" + created.getId(), String.class);
-        assertThat(response.getBody()).isNotEmpty();
-    }
+	    ResponseEntity<String> response = template.postForEntity(base.toString(), request, String.class);
+	    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+	}
 
-    Cat create(String name) {
-        Cat created = template.postForObject(base.toString(), new Cat(name, name), Cat.class);
-        assertThat(created.getId()).isNotEmpty();
-        assertThat(created.getName()).isEqualTo(name);
-        return created;
-    }
+	@Test
+	public void allShouldWork() throws Exception {
+		Collection items = template.getForObject(base.toString(), Collection.class);
+		assertThat(items.size()).isGreaterThanOrEqualTo(7);
+	}
+
+	@Test
+	public void getShouldWork() throws Exception {
+		Cat created = create("Test 1");
+		ResponseEntity<String> response = template.getForEntity(base.toString() + "/" + created.getId(), String.class);
+		assertThat(response.getBody()).isNotEmpty();
+	}
+
+	@Test
+	public void getShouldFail() throws Exception {
+		ResponseEntity<String> response = template.getForEntity(base.toString() + "/" + "invalid id", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+	}
+
+	@Test
+	public void deleteShouldWork() throws Exception {
+		Cat created = create("Test 2");
+		ResponseEntity<Void> response = template.exchange(base.toString() + "/" + created.getId(),
+				org.springframework.http.HttpMethod.DELETE, null, Void.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+	}
+
+	@Test
+	public void deleteShouldFail() throws Exception {
+		ResponseEntity<Void> response = template.exchange(base.toString() + "/" + "invalid id",
+				org.springframework.http.HttpMethod.DELETE, null, Void.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+	}
+
+	@Test
+	public void updateShouldWork() throws Exception {
+		Cat catToUpdate = create("Test 3");
+		catToUpdate.setName("Updated cat");
+		catToUpdate.setDescription("Updated description");
+		ResponseEntity<Cat> response = template.exchange(base.toString() + "/" + catToUpdate.getId(), HttpMethod.PUT,
+				new HttpEntity<>(catToUpdate), Cat.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody().getName()).isEqualTo(catToUpdate.getName());
+		assertThat(response.getBody().getDescription()).isEqualTo(catToUpdate.getDescription());
+	}
+
+	@Test
+	public void updateShouldFail() throws Exception {
+		Cat catToUpdate = create("Test 4");
+		catToUpdate.setId("invalid id");
+		ResponseEntity<Void> response = template.exchange(base.toString() + "/" + catToUpdate.getId(), HttpMethod.PUT,
+				new HttpEntity<>(catToUpdate), Void.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+	}
+
+	Cat create(String name) {
+		Cat created = template.postForObject(base.toString(), new Cat(name, name), Cat.class);
+		assertThat(created.getId()).isNotEmpty();
+		assertThat(created.getName()).isEqualTo(name);
+		return created;
+	}
 }
--- src/test/java/cx/catapult/animals/web/CatsControllerTest.java
+++ src/test/java/cx/catapult/animals/web/CatsControllerTest.java
@@ -1,17 +1,24 @@
 package cx.catapult.animals.web;
 
-import cx.catapult.animals.domain.Cat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import cx.catapult.animals.domain.Cat;
+import cx.catapult.animals.exception.AnimalNotFoundException;
+import cx.catapult.animals.service.CatsService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -19,6 +26,9 @@ public class CatsControllerTest {
 
     @Autowired
     private MockMvc mvc;
+    
+    @MockBean
+    private CatsService service;
 
     private Cat cat = new Cat("Tom", "Bob cat");
     private String json = "{ \"name\": \"Tom\", \"description\": \"Bob cat\" }";
@@ -31,13 +41,82 @@ public class CatsControllerTest {
 
     @Test
     public void get() throws Exception {
+    	cat.setId("123");
+    	
+    	when(service.get("123")).thenReturn(cat);
+    	
         mvc.perform(MockMvcRequestBuilders.get("/api/1/cats/123").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
 
     @Test
+    public void getFail() throws Exception {
+    	cat.setId("123");
+    	
+    	when(service.get("123")).thenThrow(AnimalNotFoundException.class);
+    	
+        mvc.perform(MockMvcRequestBuilders.get("/api/1/cats/123").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+    
+    @Test
     public void create() throws Exception {
         mvc.perform(MockMvcRequestBuilders.post("/api/1/cats").content(json).contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isCreated());
     }
+    
+    @Test
+    public void createFail() throws Exception {
+    	doThrow(IllegalArgumentException.class).when(service).create(any(Cat.class));
+    	
+    	
+    	mvc.perform(MockMvcRequestBuilders.post("/api/1/cats").content(json).contentType(MediaType.APPLICATION_JSON_VALUE))
+    	.andExpect(status().isBadRequest());
+    }
+    
+    @Test
+    public void getByName() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/api/1/cats/search?name=Tom").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getByDescription() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/api/1/cats/search?description=Bob cat").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void update() throws Exception {
+    	Cat updatedCat = new Cat("Tommy", "Bob cat updated");
+    	updatedCat.setId("123");
+    	
+    	when(service.update("123", updatedCat)).thenReturn(updatedCat);
+    	
+        String json = "{ \"name\": \"Tommy\", \"description\": \"Bob cat updated\" }";
+        mvc.perform(MockMvcRequestBuilders.put("/api/1/cats/123").content(json).contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk());
+    }
+    
+    @Test
+    public void updateFail() throws Exception {    	
+    	when(service.update(eq("123"), any(Cat.class))).thenThrow(AnimalNotFoundException.class);
+
+    	
+    	String json = "{ \"name\": \"Tommy\", \"description\": \"Bob cat updated\" }";
+    	mvc.perform(MockMvcRequestBuilders.put("/api/1/cats/123").content(json).contentType(MediaType.APPLICATION_JSON_VALUE))
+    	.andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void delete() throws Exception {
+    	cat.setId("123");
+    	
+    	doNothing().when(service).delete("123");
+    	
+        mvc.perform(MockMvcRequestBuilders.delete("/api/1/cats/123").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+    }
+    
+    
 }
