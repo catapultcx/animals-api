a4c105864e21420818687813bddd93e003c38ce3
wisniewskib <bartoszwisniewski185@gmail.com>
2023-03-23 16:07:52 +0000

    feat: Add error handling to service layer

--- src/main/java/cx/catapult/animals/service/BaseService.java
+++ src/main/java/cx/catapult/animals/service/BaseService.java
@@ -1,33 +1,48 @@
 package cx.catapult.animals.service;
 
 import cx.catapult.animals.domain.Animal;
+import cx.catapult.animals.exception.AnimalNotFoundException;
 
 import java.util.*;
 
 public abstract class BaseService<T extends Animal> implements Service<T> {
 
-    private HashMap<String, T> items = new HashMap<>();
+	private HashMap<String, T> items = new HashMap<>();
 
-    @Override
-    public Collection<T> all() {
-        return items.values();
-    }
+	@Override
+	public Collection<T> all() {
+		return items.values();
+	}
 
-    @Override
-    public T create(T animal) {
-        String id = UUID.randomUUID().toString();
-        animal.setId(id);
-        items.put(id, animal);
-        return animal;
-    }
+	@Override
+	public T create(T animal) {
+		String id = UUID.randomUUID().toString();
+		String name = animal.getName();
+		String description = animal.getDescription();
 
-    @Override
-    public T get(String id) {
-        return items.get(id);
-    }
-    
-    @Override
-    public void delete(String id) {
-    	items.remove(id);
-    }
+		if (name.isBlank() || description.isBlank()) {
+			throw new IllegalArgumentException("Name and description are required fields");
+		}
+
+		animal.setId(id);
+		items.put(id, animal);
+		return animal;
+	}
+
+	@Override
+	public T get(String id) {
+		T animal = items.get(id);
+		if (animal == null) {
+			throw new AnimalNotFoundException(id);
+		}
+		return animal;
+	}
+
+	@Override
+	public void delete(String id) {
+		T animal = items.remove(id);
+		if (animal == null) {
+			throw new AnimalNotFoundException(id);
+		}
+	}
 }
